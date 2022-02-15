e2aeec7783966e208e3f4fd6ef9eaef446bcafe4
natsuki-e <natsuki.endo@arsaga.jp>
2022-02-15 08:46:23 +0000

    changed graph

--- charttest/src/App.vue
+++ charttest/src/App.vue
@@ -2,7 +2,8 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <Chart :chartData="chartItems" :options="chartOptions"/>
+    <Chart :chart-data="datacollection"/>
+    <button @click="fillData()">Randomize</button>
   </div>
   
 </template>
@@ -10,59 +11,58 @@
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
 // import Chart from './components/Chart.vue'
-import axios from 'packs/axios'
+// import axios from 'packs/axios'
 import Chart from './chartbox'
+// import chartbox from './chartbox'
 
 export default {
-  name: 'App',
-  components: {
-    // HelloWorld,
-    Chart
-  },
-  data() {
-    return {
-      chartItems: {
-          labels: ["1:41", "3:21"],
-          datasets: [{
-            lineTension: 0,
-            label: "視聴維持率",
-            data: [95, 70, 80, 65, 69, 80, 100, 100, 72, 81, 45, 70,75,55],
-            backgroundColor: 'lightblue'
-          }]
-      },
-      // 後程オプションを記述
-      chartOptions: {
-        maintainAspectRatio: false,
-        tooltips: {
-            // mode: 'nearest'
-        },
-        scales: {
-          xAxes: [{
-            display: true,
-            // X軸グリッド非表示
-            gridLines: {
-              display:false
-            },
-            ticks: {
-                maxTicksLimit: 100
+    name: 'App',
+    components: {
+        // HelloWorld,
+        Chart
+    },
+    data() {
+            return {
+                datacollection: null,
+                videolength:null,
+                labels: [],
+                Percentages: [],
+                data: [],
             }
-          }],
-          // Y軸
-          yAxes: [{
-            display: true,
-            position: 'right',
-            ticks: {
-              // 0から始まる
-              beginAtZero: true,
-              // 最大5目盛
-              maxTicksLimit: 5,
-            },
-          }]
-        },
+    },
+    mounted(){
+        this.fillData()
+    },
+    methods: {
+      fillData () {
+        this.labels = []
+        this.data = []
+        this.videolength = this.getRandomIntforTime()
+        for(var i=0;i<this.videolength;i += 30){
+            this.labels.push(i)
+        }
+        for(var m=0;m<this.videolength-1;m++){
+            this.Percentages.push(this.getRandomInt())
+        }
+        this.datacollection = {
+            lineTension: 0,
+            labels: this.labels,
+            datasets: [{
+                lineTension: 0,
+                data: this.Percentages
+            }]
+        }
+        // this.datacollection.labels = this.videolength
+      },
+      getRandomInt () {
+        return Math.floor(Math.random() * 100)
+      },
+      getRandomIntforTime () {
+        return Math.floor(Math.random() * 300)
       },
     }
-  }
 }
+
 </script>
 
 <style>
